e1e91997271ad44e319f517e0602a6bb63849602
Anya <vann2299@yandex.ru>
2024-06-09 15:31:17 +0300

    fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,46 +5,23 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 import cors from 'cors';
-import router from './routes/index';
-import userRouter from './routes/user';
-import cardRouter from './routes/card';
-import { errorLogger, requestLogger } from './middlewares/logger';
-import { createUserValidation, loginValidation } from './validators/userValidator';
-import { createUser, loginUser } from './controllers/user';
-import { authMiddleware } from './middlewares/auth';
 import errorHandler from './middlewares/errorHandler';
 import { DB_ADDRESS } from './config';
+import routes from './routes';
 
 const helmet = require('helmet');
 
 const { PORT = 3001 } = process.env;
 const app = express();
 app.use(helmet());
+
 mongoose.connect(DB_ADDRESS);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-app.use(requestLogger);
-
-app.post('/signup', createUserValidation, createUser);
-app.post('/signin', loginValidation, loginUser);
-
-app.use(authMiddleware);
-
-app.use(userRouter);
-app.use(cardRouter);
-app.use(router);
-
-app.use(errorLogger);
+app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
