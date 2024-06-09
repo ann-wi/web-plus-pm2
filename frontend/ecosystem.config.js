f7afed4417b3ab400fd75c4e6c25155d2e64e6c8
Anya <vann2299@yandex.ru>
2024-06-09 14:29:30 +0300

    adding frontend deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,7 +6,7 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF,
-  DEPLOY_REPOSITORY = "origin/master",
+  DEPLOY_REPOSITORY,
 } = process.env;
 
 module.exports = {
@@ -17,7 +17,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "post-deploy": "cd frontend && pwd && npm i && npm run build",
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
     },
   },
 };
